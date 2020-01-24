1b9ccd0968417278c3f1897cd8b768c8dd4f2134
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-01-24 14:39:12 +0000

    init

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_namespace_packages
+
+requirements = ["brainrender", "napari"]
+
+
+setup(
+    name="neuro",
+    version="0.0.1a",
+    description="Visualisation and analysis of brain imaging data",
+    install_requires=requirements,
+    extras_require={
+        "dev": [
+            "sphinx",
+            "recommonmark",
+            "sphinx_rtd_theme",
+            "pydoc-markdown",
+            "black",
+            "pytest-cov",
+            "pytest",
+            "gitpython",
+            "coveralls",
+            "coverage<=4.5.4",
+        ]
+    },
+    python_requires=">=3.6",
+    packages=find_namespace_packages(exclude=("docs", "tests*")),
+    include_package_data=True,
+    url="https://github.com/SainsburyWellcomeCentre/neuro",
+    author="Adam Tyson",
+    author_email="adam.tyson@ucl.ac.uk",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+    ],
+    zip_safe=False,
+)
