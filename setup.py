9cc2449e97a79f8c4d38fa24426d088956ae765c
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-01-27 12:58:03 +0000

    refactor

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
             "coverage<=4.5.4",
         ]
     },
-    python_requires=[">=3.6", "<3.8",],
+    python_requires=">=3.6, <3.8",
     packages=find_namespace_packages(exclude=("docs", "tests*")),
     include_package_data=True,
     url="https://github.com/SainsburyWellcomeCentre/neuro",
