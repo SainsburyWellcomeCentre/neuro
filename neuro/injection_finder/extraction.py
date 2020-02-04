567c8250fec6c70387e7359f599fb79541234672
Federico Claudi <federicoclaudi@protonmail.com>
2020-02-04 11:14:09 +0000

    initial inj finder commit (#11)

---
+++ neuro/injection_finder/extraction.py
@@ -0,0 +1,198 @@
+import os
+import numpy as np
+
+from skimage.filters import gaussian as gaussian_filter
+from skimage.filters import threshold_otsu
+from skimage import measure
+
+from brainio import brainio
+
+from registration import get_registered_image
+from utils import reorient_image, marching_cubes_to_obj, get_largest_component
+from parsers import extraction_parser
+
+# For logging
+import logging
+from fancylog import fancylog
+import fancylog as package
+
+
+class Extractor:
+    def __init__(
+        self,
+        img_filepath,
+        registration_folder,
+        logging,
+        overwrite=False,
+        gaussian_kernel=2,
+        percentile_threshold=99.95,
+        threshold_type="otsu",
+        obj_path=None,
+        overwrite_registration=False,
+    ):
+
+        """
+            Extractor processes a downsampled.nii image to extract the location of the injection site.
+            This is done by registering the image to the allen CCF, blurring, thresholding and finally a 
+            marching cube algorithm to extract the surface of the injection site. 
+
+            :param img_filepath: str, path to .nii file
+            :param registration_folder: str, path to the registration folder [from cellfinder or amap]
+            :param logging: instance of fancylog logger
+            :param overwrite: bool, if False it will avoid overwriting files
+            :gaussian_kernel: float, size of kernel used for smoothing
+            :param percentile_threshold: float, in range [0, 1] percentile to use for thresholding
+            :param threshold_type: str, either ['otsu', 'percentile'], type of threshold used
+            :param obj_path: path to .obj file destination. 
+            :param overwrite_registration: if false doesn't overwrite the registration step
+        """
+
+        # Get arguments
+        self.img_filepath = img_filepath
+        self.registration_folder = registration_folder
+        self.logging = logging
+        self.overwrite = overwrite
+        self.gaussian_kernel = gaussian_kernel
+        self.percentile_threshold = percentile_threshold
+        self.threshold_type = threshold_type
+        self.obj_path = obj_path
+        self.overwrite_registration = overwrite_registration
+
+        # Run first with the image oriented for brainrender
+        image = self.setup()
+        self.extract(image)
+
+    def setup(self):
+        if not os.path.isfile(self.img_filepath):
+            raise FileNotFoundError(
+                "The image path {} is not valid".format(self.img_filepath)
+            )
+
+        self.thresholded_savepath = (
+            self.img_filepath.split(".")[0] + "_thresholded.nii"
+        )
+
+        # Get path to obj file and check if it existsts
+        if self.obj_path is None:
+            self.obj_path = self.img_filepath.split(".")[0] + ".obj"
+
+        if os.path.isfile(self.obj_path) and not self.overwrite:
+            self.logging.warning(
+                "A file exists already at {}. \
+                        Analysis will not run as overwrite is set disabled".format(
+                    self.obj_path
+                )
+            )
+
+        # Load image and register
+        image = get_registered_image(
+            self.img_filepath,
+            self.registration_folder,
+            self.logging,
+            overwrite=self.overwrite_registration,
+        )
+        return image
+
+    def extract(self, image, voxel_size=10):
+        self.logging.info("Processing " + self.img_filepath)
+        self.logging.info(
+            "Gaussian filtering with kernel size: {}".format(
+                self.gaussian_kernel
+            )
+        )
+
+        # Gaussian filter
+        kernel_shape = [self.gaussian_kernel, self.gaussian_kernel, 6]
+        filtered = gaussian_filter(image, kernel_shape)
+        self.logging.info("Filtering completed")
+
+        # Thresholding
+        if self.threshold_type.lower() == "otsu":
+            thresh = threshold_otsu(filtered)
+            self.logging.info(
+                "Thresholding with {} threshold type".format(
+                    self.threshold_type
+                )
+            )
+
+        elif (
+            self.threshold_type.lower() == "percentile"
+            or self.threshold_type.lower() == "perc"
+        ):
+            thresh = np.percentile(filtered.ravel(), self.percentile_threshold)
+            self.logging.info(
+                "Thresholding with {} threshold type. {}th percentile [{}]".format(
+                    self.threshold_type, self.percentile_threshold, thresh
+                )
+            )
+        else:
+            raise valueError(
+                "Unrecognised thresholding type: " + self.threshold_type
+            )
+
+        binary = filtered > thresh
+        oriented_binary = reorient_image(
+            binary, invert_axes=[2,], orientation="coronal"
+        )
+
+        # Save thresholded image
+        if not os.path.isfile(self.thresholded_savepath) or self.overwrite:
+            self.logging.info(
+                "Saving thresholded image to {}".format(
+                    self.thresholded_savepath
+                )
+            )
+            brainio.to_nii(binary.astype(np.int16), self.thresholded_savepath)
+
+        # apply marching cubes
+        self.logging.info("Extracting surface from thresholded image")
+        verts, faces, normals, values = measure.marching_cubes_lewiner(
+            oriented_binary, 0, step_size=1
+        )
+
+        # Scale to atlas spacing
+        if voxel_size is not 1:
+            verts = verts * voxel_size
+
+        # Save image to .obj
+        self.logging.info(" Saving .obj at {}".format(self.obj_path))
+        faces = faces + 1
+        marching_cubes_to_obj((verts, faces, normals, values), self.obj_path)
+
+        # Keep only the largest connected component
+        get_largest_component(self.obj_path)
+
+
+def main():
+    args = extraction_parser().parse_args()
+
+    # Get output directory
+    if args.output_directory is None:
+        outdir = os.get_cwd()
+    elif not os.path.isdir(args.output_directory):
+        raise ValueError("Output directory invalid")
+    else:
+        outdir = args.output_directory
+
+    # Start log
+    log_name = "injection_finder_{}".format(
+        os.path.split(args.registration_folder)[-1]
+    )
+    fancylog.start_logging(outdir, package, filename=log_name, verbose=True)
+
+    # Start extraction
+    Extractor(
+        args.img_filepath,
+        args.registration_folder,
+        logging,
+        overwrite=args.overwrite,
+        gaussian_kernel=args.gaussian_kernel,
+        percentile_threshold=args.percentile_threshold,
+        threshold_type=args.threshold_type,
+        obj_path=args.obj_path,
+        overwrite_registration=args.overwrite_registration,
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ neuro/injection_finder/registration.py
@@ -0,0 +1,76 @@
+import os
+
+from brainio import brainio
+from amap.tools import source_files
+from amap.config.config import get_binary
+from amap.tools.exceptions import RegistrationError
+
+
+from cellfinder.tools.system import (
+    safe_execute_command,
+    SafeExecuteCommandError,
+)
+
+PROGRAM_NAME = "reg_resample"
+DEFAULT_CONTROL_POINT_FILE = "inverse_control_point_file.nii"
+default_atlas_name = "brain_filtered.nii"
+
+
+def prepare_segmentation_cmd(
+    program_path,
+    floating_image_path,
+    output_file_name,
+    destination_image_filename,
+    control_point_file,
+):
+    cmd = "{} -cpp {} -flo {} -ref {} -res {}".format(
+        program_path,
+        control_point_file,
+        floating_image_path,
+        destination_image_filename,
+        output_file_name,
+    )
+    return cmd
+
+
+def get_registered_image(nii_path, registration_dir, logging, overwrite=False):
+    # get binaries
+    nifty_reg_binaries_folder = source_files.get_niftyreg_binaries()
+    program_path = get_binary(nifty_reg_binaries_folder, PROGRAM_NAME)
+
+    # get file paths
+    basedir = os.path.split(nii_path)[0]
+    output_filename = os.path.join(
+        basedir,
+        "{}_transformed.nii".format(os.path.split(nii_path)[1].split(".")[0]),
+    )
+    if os.path.isfile(output_filename) and not overwrite:
+        run = False
+    else:
+        run = True
+
+    if run:
+        destination_image = os.path.join(registration_dir, default_atlas_name)
+        control_point_file = os.path.join(
+            registration_dir, DEFAULT_CONTROL_POINT_FILE
+        )
+
+        log_file_path = os.path.join(basedir, "registration_log.txt")
+        error_file_path = os.path.join(basedir, "registration_err.txt")
+
+        reg_cmd = prepare_segmentation_cmd(
+            program_path,
+            nii_path,
+            output_filename,
+            destination_image,
+            control_point_file,
+        )
+        logging.info("Running registration")
+        try:
+            safe_execute_command(reg_cmd, log_file_path, error_file_path)
+        except SafeExecuteCommandError as err:
+            raise RegistrationError("Registration failed; {}".format(err))
+    else:
+        logging.info("Skipping registration as output file already exists")
+
+    return brainio.load_any(output_filename)
