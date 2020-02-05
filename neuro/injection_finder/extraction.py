7658f7388af1c31fcddc25ce94c45294a1f8daa4
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-02-05 16:42:01 +0000

    keep largest object

--- neuro/injection_finder/extraction.py
+++ neuro/injection_finder/extraction.py
@@ -138,6 +138,7 @@ class Extractor:
             )
 
         binary = filtered > thresh
+        binary = keep_n_largest_objects(binary)
         oriented_binary = reorient_image(
             binary, invert_axes=[2,], orientation="coronal"
         )
@@ -166,9 +167,42 @@ class Extractor:
         faces = faces + 1
         marching_cubes_to_obj((verts, faces, normals, values), self.obj_path)
 
-        # Keep only the largest connected component
-        # DO THIS AT THE IMAGE STAGE
-        # get_largest_component(self.obj_path)
+
+def keep_n_largest_objects(numpy_array, n=1, connectivity=None):
+    """
+    Given an input binary numpy array, return a "clean" array with only the
+    n largest connected components remaining
+
+    Inspired by stackoverflow.com/questions/47540926
+
+    TODO: optimise
+
+    :param numpy_array: Binary numpy array
+    :param n: How many objects to keep
+    :param connectivity: Labelling connectivity (see skimage.measure.label)
+    :return: "Clean" numpy array with n largest objects
+    """
+
+    labels = measure.label(numpy_array, connectivity=connectivity)
+    assert labels.max() != 0  # assume at least 1 CC
+    n_largest_objects = get_largest_non_zero_object(labels)
+    if n > 1:
+        i = 1
+        while i < n:
+            labels[n_largest_objects] = 0
+            n_largest_objects += get_largest_non_zero_object(labels)
+            i += 1
+    return n_largest_objects
+
+
+def get_largest_non_zero_object(label_image):
+    """
+    In a labelled (each object assigned an int) numpy array. Return the
+    largest object with a value >= 1.
+    :param label_image: Output of skimage.measure.label
+    :return: Boolean numpy array or largest object
+    """
+    return label_image == np.argmax(np.bincount(label_image.flat)[1:]) + 1
 
 
 def main():
