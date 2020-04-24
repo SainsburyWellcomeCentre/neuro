c2ad0ca33f36c4831e0a234457baa94e52fd03b7
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-04-24 16:54:18 +0100

    bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requirements = [
 
 setup(
     name="neuro",
-    version="0.0.9",
+    version="0.0.10",
     description="Visualisation and analysis of brain imaging data",
     install_requires=requirements,
     extras_require={
